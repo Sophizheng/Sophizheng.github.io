d2aea41fcccd7dd810601c0415e53e443060df05
Sophia <Sophizheng0722@berkeley.edu>
2023-04-06 12:11:40 -0700

    hw7

---
+++ homeworks/hw7/assets/css/script.js
@@ -0,0 +1,29 @@
+function averageThreeNumbers(a, b, c) {
+    let sum = a + b + c;
+    let average = sum / 3;
+    return average;
+}
+
+function createSentence (num, noun) {
+    let res = "On average, a Berkeley student has " + num + " " + noun + "s";
+    return res;
+
+}
+
+function getRandomNum (max) {
+
+    let res = Math.floor(Math.random() * (max + 1));
+    return res;
+
+}
+
+let x = getRandomNum(20);
+let y = getRandomNum(10);
+let z = getRandomNum(13);
+
+let avg = averageThreeNumbers(x,y,z);
+
+let sentence = createSentence(avg, "panda");
+
+console.log(sentence) ;
+
